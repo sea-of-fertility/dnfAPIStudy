fd9ba4172e7317451efa152878546b2ac43b0a33
LetgoMoveNotLate <hj981203@gmail.com>
2023-12-06 16:53:18 +0900

    refactor: controller 계층 단순화 controller 계층에 있는 restTemplate 사용 메소드를 service 계층으로 이동시켰다.

--- src/main/java/com/example/dnfapistudy/controller/CharaController.java
+++ src/main/java/com/example/dnfapistudy/controller/CharaController.java
@@ -2,22 +2,14 @@ package com.example.dnfapistudy.controller;
 
 
 import com.example.dnfapistudy.api.CharaBasicInform;
-import com.example.dnfapistudy.api.CharaBasicInform.CharacterInform;
-import com.example.dnfapistudy.domain.Character;
 import com.example.dnfapistudy.request.FindCharacter;
 import com.example.dnfapistudy.service.CharaService;
-import com.example.dnfapistudy.service.FileSystemStorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 
@@ -26,16 +18,8 @@ import java.io.IOException;
 @Slf4j
 public class CharaController {
 
-    private final RestTemplate restTemplate;
-    private final FileSystemStorageService fileSystemStorageService;
+
     private final CharaService charaService;
-
-    @Value("${api-key}")
-    private String apiKey;
-    private final int imageSize = 1;
-    private final String imageUrl = "https://img-api.neople.co.kr/df/servers/%s/characters/%s?zoom=%s";
-    private final String charaSearch = "https://api.neople.co.kr/df/servers/%s/characters?characterName=%s&jobId=%s&jobGrowId=%s&isAllJobGrow=%s&limit=<limit>&wordType=match&apikey=%s";
-
 
     /**
      *
@@ -45,40 +29,8 @@ public class CharaController {
      */
     @GetMapping("/character")
     public void chara(@RequestBody FindCharacter findCharacter) throws IOException {
-        CharaBasicInform charaBasicInform = findCharacterId(findCharacter);
-        setImage(charaBasicInform);
-//        return charaBasicInform;
+        CharaBasicInform charaBasicInform = charaService.findCharacterId(findCharacter);
+        charaService.setImageAndSave(charaBasicInform);
     }
-
-    /**
-     *
-     * @param findCharacter
-     * @return 검색된 모든 캐릭어의 고유한 id값을 []형태로 받아온다..
-     */
-    private CharaBasicInform findCharacterId(FindCharacter findCharacter) {
-        return restTemplate.getForObject(
-                String.format("https://api.neople.co.kr/df/servers/%s/characters?characterName=%s&apikey=%s",
-                        findCharacter.getServerId(), findCharacter.getName(), apiKey),
-                CharaBasicInform.class);
-    }
-
-
-    /**
-     *
-     * @param charaBasicInform
-     * @throws IOException
-     */
-    private void setImage(CharaBasicInform charaBasicInform) throws IOException {
-        CharacterInform[] rows = charaBasicInform.getRows();
-        for (CharacterInform row : rows) {
-            byte[] forObject = restTemplate.getForObject(String.format(imageUrl, row.getServerId(), row.getCharacterId(), imageSize), byte[].class);
-            assert forObject != null;
-            BufferedImage img = ImageIO.read(new ByteArrayInputStream(forObject));
-            String store = fileSystemStorageService.store(img, row.getCharacterId()).toString();
-            Character entity = row.toEntity(store);
-            charaService.save(entity);
-        }
-    }
-
 
 }
--- src/main/java/com/example/dnfapistudy/service/CharaService.java
+++ src/main/java/com/example/dnfapistudy/service/CharaService.java
@@ -1,20 +1,67 @@
 package com.example.dnfapistudy.service;
 
+import com.example.dnfapistudy.api.CharaBasicInform;
 import com.example.dnfapistudy.domain.Character;
 import com.example.dnfapistudy.reposiotry.CharacterRepository;
+import com.example.dnfapistudy.request.FindCharacter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class CharaService {
 
+
+    private final FileSystemStorageService fileSystemStorageService;
+    private final RestTemplate restTemplate;
     private final CharacterRepository characterRepository;
 
-
-
+    @Value("${api-key}")
+    private String apiKey;
+    private final int imageSize = 1;
+    private final String imageUrl = "https://img-api.neople.co.kr/df/servers/%s/characters/%s?zoom=%s";
+    private final String charaSearch = "https://api.neople.co.kr/df/servers/%s/characters?characterName=%s&jobId=%s&jobGrowId=%s&isAllJobGrow=%s&limit=<limit>&wordType=match&apikey=%s";
 
     public void save(Character character){
         characterRepository.save(character);
     }
+
+
+    /**
+     *
+     * @param findCharacter
+     * @return 검색된 모든 캐릭어의 고유한 id값을 []형태로 받아온다..
+     */
+    public CharaBasicInform findCharacterId(FindCharacter findCharacter) {
+        return restTemplate.getForObject(
+                String.format("https://api.neople.co.kr/df/servers/%s/characters?characterName=%s&apikey=%s",
+                        findCharacter.getServerId(), findCharacter.getName(), apiKey),
+                CharaBasicInform.class);
+    }
+
+
+    /**
+     *
+     * @param charaBasicInform
+     * @throws IOException
+     */
+    public void setImageAndSave(CharaBasicInform charaBasicInform) throws IOException {
+        CharaBasicInform.CharacterInform[] rows = charaBasicInform.getRows();
+        for (CharaBasicInform.CharacterInform row : rows) {
+            byte[] forObject = restTemplate.getForObject(String.format(imageUrl, row.getServerId(), row.getCharacterId(), imageSize), byte[].class);
+            assert forObject != null;
+            BufferedImage img = ImageIO.read(new ByteArrayInputStream(forObject));
+            String imagePath = fileSystemStorageService.store(img, row.getCharacterId()).toString();
+            Character entity = row.toEntity(imagePath);
+            save(entity);
+        }
+    }
+
 }
